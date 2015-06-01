2fa3256912740370e63c845da0c1211fb330f1e8
pablojuancruz <pablojuan@foryoursystem.com>
2015-05-31 22:41:31 -0300

    Primeiro Commit

---
+++ ExemploBDD.Specs/ConvTemperatura.feature
@@ -0,0 +1,27 @@
+﻿Funcionalidade: Conversão de Fahrenheit para outras escalas
+	Ao informar uma temperatura em Fahrenheit
+	Eu quero obter os valores equivalentes nas escalas Celsius e Kelvin
+
+Cenário: Conversao 01
+	Dado que a temperatura em Fahrenheit é de 32 graus
+	Quando eu solicitar a conversão da temperatura
+	Então a temperatura na escala Celsius será de 0 graus
+	E a temperatura na escala Kelvin será de 273,15 graus
+
+Cenário: Conversao 02
+	Dado que a temperatura em Fahrenheit é de 86 graus
+	Quando eu solicitar a conversão da temperatura
+	Então a temperatura na escala Celsius será de 30 graus
+	E a temperatura na escala Kelvin será de 303,15 graus
+
+Cenário: Conversao 03
+	Dado que a temperatura em Fahrenheit é de 90,5 graus
+	Quando eu solicitar a conversão da temperatura
+	Então a temperatura na escala Celsius será de 32,5 graus
+	E a temperatura na escala Kelvin será de 305,65 graus
+
+Cenário: Conversao 04
+	Dado que a temperatura em Fahrenheit é de 212 graus
+	Quando eu solicitar a conversão da temperatura
+	Então a temperatura na escala Celsius será de 100 graus
+	E a temperatura na escala Kelvin será de 373,15 graus
---
+++ ExemploBDD.Specs/ConvTemperaturaStepDefinition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using ExemploBDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExemploBDD.Specs
+{
+    [Binding]
+    public sealed class ConvTemperaturaStepDefinition
+    {
+        private double _temperaturaFahrenheit;
+        private double _temperaturaCelsius;
+        private double _temperaturaKelvin;
+
+        [Given("que a temperatura em Fahrenheit é de (.*) graus")]
+        public void CarregarTemperaturaFahrenheit(double temperatura)
+        {
+            _temperaturaFahrenheit = temperatura;
+        }
+
+        [When("eu solicitar a conversão da temperatura")]
+        public void ProcessarConversaoTemperatura()
+        {
+            _temperaturaCelsius =
+                ConversorTemperatura.FahrenheitParaCelsius(_temperaturaFahrenheit);
+            _temperaturaKelvin =
+                ConversorTemperatura.FahrenheitParaKelvin(_temperaturaFahrenheit);
+        }
+
+        [Then("a temperatura na escala Celsius será de (.*) graus")]
+        public void ValidarTemperaturaCelsius(double valorEsperado)
+        {
+            Assert.AreEqual(valorEsperado, _temperaturaCelsius);
+        }
+
+        [Then("a temperatura na escala Kelvin será de (.*) graus")]
+        public void ValidarTemperaturaKelvin(double valorEsperado)
+        {
+            Assert.AreEqual(valorEsperado, _temperaturaKelvin);
+        }
+    }
+}
